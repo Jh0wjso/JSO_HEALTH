f98b69a71c55376443aec1b3e627ac8082b2dab8
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-08-11 17:39:29 -0300

    Update on statusbar.

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet,  View } from 'react-native';
+import { StyleSheet,  View, StatusBar } from 'react-native';
 import Title from './src/components/Title/';
 import Form from './src/components/Form';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <StatusBar
+        animated={true}
+        backgroundColor="#000000"
+      />
       <Title></Title>
       <Form></Form>
     </View>
@@ -17,5 +21,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'black',
     paddingTop: 80,
-  },
+  }
 });
